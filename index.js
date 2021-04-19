cf977aa45fb4140239c8bf936c7ec165a77cf23a
Exx-hub <alvinfloresacosta@gmail.com>
2021-04-19 19:10:09 +0800

    add: Database Connection Setup

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const express = require('express');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
 app.use(cors());
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-const PORT = process.env.PORT || 3000;
+// ENV VARIABLES
+const port = process.env.PORT || 3000;
+const dbUri = process.env.DB_CONN_STR;
 
+
+// MONGOOSE CONNECTION SETUP
+mongoose.connect(dbUri,{
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
+})
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => console.log("DB connection established."));
+
+// ROOT ROUTE
 app.get('/', (req,res) => {
 	res.status(200).json({
 		"message": "Course Booking System Root Route"
 	})
 })
 
+// SERVER LISTEN
 app.listen(3000, () => {
 	console.log(`Server listening on port 3000`);
 })
