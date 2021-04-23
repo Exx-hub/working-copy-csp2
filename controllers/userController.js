9edb9c8e0dd5663564bc966f3798ce493cc53eda
Exx-hub <alvinfloresacosta@gmail.com>
2021-04-23 18:38:28 +0800

    update: Return user ID to client via /api/users/login

--- controllers/userController.js
+++ controllers/userController.js
@@ -39,10 +39,14 @@ module.exports.verifyEmail = (emailObj) => {
 module.exports.login = (userData) => {
 	const {emailAddress, password} = userData;
 
-	return User.findOne({emailAddress}).then((foundUser) => {
+	return User.findOne({emailAddress}, 'password').then((foundUser) => {
+		console.dir(foundUser);
 
 		if(foundUser === null){
-			return false;
+			return {
+				data: false,
+				userDetails: "Username/Email not registered."
+			}
 		} else {
 			// user document was found
 			// check if password is the same as password in found document
@@ -50,9 +54,18 @@ module.exports.login = (userData) => {
 			const doesPasswordsMatch = bcrypt.compareSync(password, foundUser.password)
 
 			if(doesPasswordsMatch) {
-				return true;
+				let userUpdated = foundUser.toObject();
+				delete userUpdated.password
+
+				return {
+					data: true,
+					userDetails: userUpdated
+				}
 			} else {
-				return false;
+				return {
+					data: false,
+					userDetails: "Password incorrect."
+				};
 			}
 		}
 	})
