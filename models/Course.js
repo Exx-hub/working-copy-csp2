361e5ea6673d0d77a080df19cc0c647a96bb02ca
Exx-hub <alvinfloresacosta@gmail.com>
2021-04-30 23:39:37 +0800

    test course model

--- models/Course.js
+++ models/Course.js
@@ -1,16 +1,22 @@
 const mongoose = require('mongoose');
 
 const courseSchema = new mongoose.Schema({
-	name: {type: String},
-	description: {type: String},
-	price: {type: Number},
-	isActive: {type: Boolean, default: true},
-	createdOn: {type: Date, default: new Date()},
-	enrollees: [{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User'
-        }]
-})
+    name: String,
+    description: String,
+    price: Number,
+    isActive: {
+      type: Boolean,
+      default: true
+    },
+    createdOn: {
+      type: Date,
+      default: new Date()
+    },
+    enrollees: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+});
 
 
 module.exports = mongoose.model("Course", courseSchema);
