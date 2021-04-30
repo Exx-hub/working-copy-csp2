5176cd94ee7a41a8011612eb0c195c56aafe00af
Exx-hub <alvinfloresacosta@gmail.com>
2021-04-30 17:43:41 +0800

    update: Store ObjectIds in enrollees field of Course schema

--- models/Course.js
+++ models/Course.js
@@ -7,8 +7,8 @@ const courseSchema = new mongoose.Schema({
 	isActive: {type: Boolean, default: true},
 	createdOn: {type: Date, default: new Date()},
 	enrollees: [{
-		userId: {type: String},
-		lastName: {type: String}
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User"
 	}]
 })
 
